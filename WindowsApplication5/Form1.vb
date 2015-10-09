da14e570d597386ddcab1f18381b1097f70cac17
Albert Cerdà Fernandez <kisukemi@gmail.com>
2015-10-09 17:38:35 +0200

    random

---
+++ WindowsApplication5/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim min As Integer
+        Dim max As Integer
+        Dim ramd As New Random
+
+        min = CInt(TextBox1.Text)
+        max = CInt(TextBox2.Text)
+
+
+        Label1.Text = ramd.Next(min, max)
+
+
+
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
